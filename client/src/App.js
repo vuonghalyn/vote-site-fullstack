f1a17bbde5edf9bb24cc14faea5c96d788073d3d
Hang Vuong <vuonghalyn@gmail.com>
2022-09-14 22:56:10 +0300

    add three options slots with semantic-ui-react

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import _ from "lodash";
+import React, { Component, useState } from "react";
+import { Button, Card, Divider, Image, Placeholder } from "semantic-ui-react";
+
+import "./App.css";
+
+const cards = [
+  {
+    avatar: "/images/avatar/large/helen.jpg",
+    date: "Joined in 2013",
+    header: "Helen",
+    description: "Primary Contact",
+  },
+  {
+    avatar: "/images/avatar/large/matthew.png",
+    date: "Joined in 2013",
+    header: "Matthew",
+    description: "Primary Contact",
+  },
+  {
+    avatar: "/images/avatar/large/molly.png",
+    date: "Joined in 2013",
+    header: "Molly",
+    description: "Primary Contact",
+  },
+];
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  const handleLoadingClick = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button loading={loading} onClick={handleLoadingClick} primary>
+        Simulate loading
+      </Button>
+      <Divider />
+
+      <Card.Group doubling itemsPerRow={3} stackable>
+        {_.map(cards, (card) => (
+          <Card key={card.header}>
+            {loading ? (
+              <Placeholder>
+                <Placeholder.Image square />
+              </Placeholder>
+            ) : (
+              <Image src={card.avatar} />
+            )}
+
+            <Card.Content>
+              {loading ? (
+                <Placeholder>
+                  <Placeholder.Header>
+                    <Placeholder.Line length="very short" />
+                    <Placeholder.Line length="medium" />
+                  </Placeholder.Header>
+                  <Placeholder.Paragraph>
+                    <Placeholder.Line length="short" />
+                  </Placeholder.Paragraph>
+                </Placeholder>
+              ) : (
+                <>
+                  <Card.Header>{card.header}</Card.Header>
+                  <Card.Meta>{card.date}</Card.Meta>
+                  <Card.Description>{card.description}</Card.Description>
+                </>
+              )}
+            </Card.Content>
+
+            <Card.Content extra>
+              <Button disabled={loading} primary>
+                Add
+              </Button>
+              <Button disabled={loading}>Delete</Button>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    </>
   );
 }
 
